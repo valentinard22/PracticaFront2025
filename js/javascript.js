e54ddb9000ee51e2eb786e7c458ab2cc3cc664db
valentinard22 <valentinaruizdiaz@lfl.edu.ar>
2025-12-13 06:05:18 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+function byn() {
+  document.body.style.backgroundColor = "black";
+
+  document.getElementById("p").style.color = "white";
+   document.getElementById("h2").style.color = "white";
+   document.getElementById("h3").style.color = "white";
+   document.getElementById("lista").style.color = "white";
+   document.getElementById("h4").style.color = "white";
+   document.getElementById("h5").style.color = "white";
+
+}
+
+function Cartel(){alert("Autora:Valentina Ruiz Diaz Gmail: Valentinaruizdiaz@lfl.edu.ar")}
